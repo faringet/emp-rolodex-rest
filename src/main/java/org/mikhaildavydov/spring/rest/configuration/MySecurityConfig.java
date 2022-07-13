3d3da4077520187c94d54138d6252d4a35ad0ed3
faringet <mik.davydov14@gmail.com>
2022-07-13 15:49:09 +0300

    add Spring Security

---
+++ src/main/java/org/mikhaildavydov/spring/rest/configuration/MySecurityConfig.java
@@ -0,0 +1,39 @@
+package org.mikhaildavydov.spring.rest.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"))
+                .withUser(userBuilder.username("superadmin").password("superadmin").roles("SUPERADMIN"));
+
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+//                .antMatchers("/hr_info").hasRole("HR")
+//                .antMatchers("/manager_info").hasRole("MANAGER")
+//                .and().formLogin().permitAll();
+//    }
+}
---
+++ src/main/java/org/mikhaildavydov/spring/rest/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package org.mikhaildavydov.spring.rest.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
